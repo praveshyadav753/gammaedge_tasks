1507fce3b8d66afebb8a1e81a329ef81916ca19f
praveshyadav753 <pravesh.yadav.gammaedge.io>
2026-02-17 13:51:11 +0530

    llm api call and handles websocket

--- llm/chat_app/main.py
+++ llm/chat_app/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.templating import  Jinja2Templates
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
@@ -8,44 +7,49 @@ import json
 import os
 from google import genai
 from dotenv import load_dotenv
-from chat_model import get_model
+from chat_model import get_response
 
 load_dotenv()
 
 app = FastAPI()
 
-api_key = os.getenv('GEN_AI')
+api_key = os.getenv("GEN_AI")
 
 client = genai.Client(api_key=api_key)
 
-app.mount('/static',StaticFiles(directory="static"),name="static")
-templates = Jinja2Templates(directory='templates')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="template")
 
-active_connections = []
+active_connections = {}
 
-@app.get('/',response_class=HTMLResponse)
-async def get_chat(request:Request):
+
+@app.get("/", response_class=HTMLResponse)
+async def get_chat(request: Request):
     user_id = str(uuid.uuid4())
-    return templates.TemplateResponse("chat.html",{"request": request ,"user_id":user_id})
-    
+    return templates.TemplateResponse(
+        "chat.html", {"request": request, "user_id": user_id}
+    )
+
+
 @app.websocket("/ws/{user_id}")
-async def start_websocket(websocket:WebSocket,user_id:str):
+async def start_websocket(websocket: WebSocket, user_id: str):
     await websocket.accept()
-    active_connections[user_id] = WebSocket
+    active_connections[user_id] = websocket
 
     try:
         while True:
-            data = websocket.receive_text()
-            message = json.loads(data)["message"]
+            data = await websocket.receive_text()
+            try:
+                payload = json.loads(data)
+                message = payload.get("message", "")
+            except json.JSONDecodeError:
+                await websocket.send_text("Invalid message format")
+                continue
 
-            llm_res = await get_response(message)
+            async for chunk in get_response(message):
+                await websocket.send_text(  chunk)
 
-            if llm_res:
-                # await websocket.send_text() 
-                await websocket.send_text(json.dumps({"response": llm_res}))
-            else:
-                llm_res= ""
-                websocket.send_text(llm_res)    
-    except WebSocketDisconnect :
-                active_connections.pop(user_id, None)
-
+          
+    except WebSocketDisconnect:
+        active_connections.pop(user_id, None)
+        print(f"User {user_id} disconnected")
